0c5667088a94a947c537e82be40c5e2a48cc6f55
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-06-11 10:16:21 +0530

    Reversed a Number

--- BasicMaths.cpp
+++ BasicMaths.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // Function definition
+// Count the number of digits
 int count(int n)
 {
     int cnt = 0;
@@ -12,13 +13,28 @@ int count(int n)
     return cnt;
 }
 
+// Reverse a number
+int Reverse(int n)
+{
+    int ReversedNum = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        ReversedNum = ReversedNum * 10 + digit;
+        n = n / 10;
+    }
+    return ReversedNum;
+}
+
 int main()
 {
     // Call the count function and store the result
     int result = count(156);
+    int Reversed = Reverse(4567);
 
-    // Print the result
+    // Print the results
     std::cout << "The number of digits in 156 is: " << result << std::endl;
+    std::cout << "The reversed number of 4567 is: " << Reversed << std::endl;
 
     return 0;
 }
