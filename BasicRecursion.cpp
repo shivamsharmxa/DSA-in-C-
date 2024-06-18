17b9fe80e4c501bbdc63fac7097dd4089ca1cc27
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-06-18 08:39:16 +0530

    Reverse an array by One Pointer

--- BasicRecursion.cpp
+++ BasicRecursion.cpp
@@ -30,7 +30,7 @@ void print2(int i, int n)
 // Reverse an array by using 2 pointers
 void printArray(int arr[], int n)
 {
-    cout << "The reversed array is : " << endl;
+    cout << "The reversed array is by one : " << endl;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
@@ -45,11 +45,20 @@ void ReverseArray(int arr[], int start, int end)
     ReverseArray(arr, start + 1, end - 1);
 }
 
+// by using one pointer
+void ReverseArraybyOne(int i, int arr[], int n)
+{
+    if (i >= n / 2)
+        return;
+    swap(arr[i], arr[n - i - 1]);
+    ReverseArraybyOne(i + 1, arr, n);
+}
+
 int main()
 {
     int n = 5;
     int arr[] = {1, 2, 3, 4, 5};
-    ReverseArray(arr, 0, n - 1);
+    ReverseArraybyOne(0, arr, n);
     printArray(arr, n);
     return 0;
 }
