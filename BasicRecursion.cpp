8e18992386ee94b41e4dd68ac8dfc7e007fbd418
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-06-18 08:27:25 +0530

    Reverse an array using 2 pointers

--- BasicRecursion.cpp
+++ BasicRecursion.cpp
@@ -27,8 +27,29 @@ void print2(int i, int n)
     print2(i + 1, n);
 }
 
+// Reverse an array by using 2 pointers
+void printArray(int arr[], int n)
+{
+    cout << "The reversed array is : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+void ReverseArray(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+    swap(arr[start], arr[end]);
+    ReverseArray(arr, start + 1, end - 1);
+}
+
 int main()
 {
-    print2(1, 5);
+    int n = 5;
+    int arr[] = {1, 2, 3, 4, 5};
+    ReverseArray(arr, 0, n - 1);
+    printArray(arr, n);
     return 0;
 }
