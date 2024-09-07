0a0880721ebeb7ee55d9c406d3a8bcebc9e3b954
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-09-07 19:35:44 +0530

    Second Largest Number in Array

--- Array.cpp
+++ Array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 int LargestNumber(int arr[], int n)
 {
@@ -13,11 +14,41 @@ int LargestNumber(int arr[], int n)
     return largest;
 }
 
+// int main()
+// {
+//     int arr[] = {12, 23, 54, 28, 67};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     std::cout << "The largest number is " << LargestNumber(arr, n) << std::endl; // Output the largest number
+//     return 0;
+// }
+
+// Second largest Number in Array
+int SlargestNumber(int arr[], int n)
+{
+    int largest = arr[0];
+    int slargest = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            slargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > slargest)
+        {
+            slargest = arr[i];
+        }
+    }
+    return slargest;
+}
+
 int main()
 {
     int arr[] = {12, 23, 54, 28, 67};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    std::cout << "The largest number is " << LargestNumber(arr, n) << std::endl; // Output the largest number
+    std::cout << "The Second largest number is " << SlargestNumber(arr, n) << std::endl; // Output the largest number
     return 0;
 }
