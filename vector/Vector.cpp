f1374e649b7322f442369ae9892cc8213f0cec87
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-11-13 16:38:17 +0530

    Sorting in vector

--- vector/Vector.cpp
+++ vector/Vector.cpp
@@ -21,33 +21,63 @@ int main()
     // cout << "Capacity of the vector :" << v1.capacity() << endl;
 
     // Delete the value from the vector
-    vector<int> vnew;
-    vnew.push_back(4);
-    vnew.push_back(45);
-    vnew.push_back(67);
-    vnew.push_back(98);
-    vnew.push_back(78);
-    vnew.pop_back();
+    //
 
-    cout << " Size of the Vector :" << vnew.size() << endl;
-    cout << " Capacity of the Vector :" << vnew.capacity() << endl;
+    // print first and last elements in the vector
 
-    vnew.erase(vnew.begin() + 3);
-    cout << "After removing the element" << endl;
-    cout << " Size of the Vector :" << vnew.size() << endl;
-    cout << " Capacity of the Vector :" << vnew.capacity() << endl;
+    vector<int> arr;
 
-    for (int i = 0; i < vnew.size(); i++)
+    arr.push_back(34);
+    arr.push_back(65);
+    arr.push_back(312);
+    arr.push_back(44);
+    arr.push_back(64);
+
+    cout << arr[0] << endl;
+    cout << arr.front() << endl;
+    cout << arr[arr.size() - 1] << endl;
+    cout << arr.back() << endl;
+
+    // copy the elements of first vector to another
+
+    // vector<int> a;
+
+    // a = arr;
+    // cout << a.size();
+    // cout << endl;
+
+    // for (auto it = arr.begin(); it != arr.end(); it++)
+    // {
+    //     cout << *it << " ";
+    // }
+    // cout << endl;
+    // for (auto i : arr)
+    //     cout << i << " ";
+
+    // sorting in Vector
+
+    vector<int> ans;
+
+    ans.push_back(10);
+    ans.push_back(78);
+    ans.push_back(40);
+    ans.push_back(3);
+    ans.push_back(13);
+
+    // sort in increasing order
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++)
     {
-        cout << vnew[i] << " " << endl;
+        cout << ans[i] << endl;
+        cout << endl;
     }
 
-    // insert value
-    vnew.insert(vnew.begin() + 1, 69);
-    cout << endl;
-
-    for (int i = 0; i < vnew.size(); i++)
+    // sort in decreasing order
+    cout << "sort in decreasing order" << endl;
+    sort(ans.rbegin(), ans.rend());
+    for (int i = 0; i < ans.size(); i++)
     {
-        cout << vnew[i] << " " << endl;
+        cout << ans[i] << endl;
+        cout << endl;
     }
 }
