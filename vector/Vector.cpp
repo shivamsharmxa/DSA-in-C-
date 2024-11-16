b2aa4c348693b2b44184c1d5adb5a9213c283025
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-11-16 15:12:45 +0530

    Prefix and Sufix

--- vector/Vector.cpp
+++ vector/Vector.cpp
@@ -3,81 +3,120 @@
 
 using namespace std;
 
+// int main()
+// {
+//     // create a vector
+//     // vector<int> v;
+//     // vector<int> v1(5, 2);
+
+//     // cout << "Size of the vector :" << v.size() << endl;
+//     // cout << "Capacity of the vector :" << v.capacity() << endl;
+
+//     // v.push_back(2);
+//     // v.push_back(3);
+//     // v.push_back(4);
+//     // v1.push_back(7);
+
+//     // cout << "Size of the vector :" << v1.size() << endl;
+//     // cout << "Capacity of the vector :" << v1.capacity() << endl;
+
+//     // Delete the value from the vector
+//     //
+
+//     // print first and last elements in the vector
+
+//     vector<int> arr;
+
+//     arr.push_back(34);
+//     arr.push_back(65);
+//     arr.push_back(312);
+//     arr.push_back(44);
+//     arr.push_back(64);
+
+//     cout << arr[0] << endl;
+//     cout << arr.front() << endl;
+//     cout << arr[arr.size() - 1] << endl;
+//     cout << arr.back() << endl;
+
+//     // copy the elements of first vector to another
+
+//     // vector<int> a;
+
+//     // a = arr;
+//     // cout << a.size();
+//     // cout << endl;
+
+//     // for (auto it = arr.begin(); it != arr.end(); it++)
+//     // {
+//     //     cout << *it << " ";
+//     // }
+//     // cout << endl;
+//     // for (auto i : arr)
+//     //     cout << i << " ";
+
+//     // sorting in Vector
+
+//     vector<int> ans;
+
+//     ans.push_back(10);
+//     ans.push_back(78);
+//     ans.push_back(40);
+//     ans.push_back(3);
+//     ans.push_back(13);
+
+//     // sort in increasing order
+//     sort(ans.begin(), ans.end());
+//     for (int i = 0; i < ans.size(); i++)
+//     {
+//         cout << ans[i] << endl;
+//         cout << endl;
+//     }
+
+//     // sort in decreasing order
+//     cout << "sort in decreasing order" << endl;
+//     sort(ans.rbegin(), ans.rend());
+//     for (int i = 0; i < ans.size(); i++)
+//     {
+//         cout << ans[i] << endl;
+//         cout << endl;
+//     }
+// }
+
+// Prefix and Suffix
+
+int divide(vector<int> arr)
+{
+    int Maxi = INT_MIN, prefix = 0, total_sum = 0, n = arr.size();
+
+    // total sum
+    for (int i = 0; i < n; i++)
+    {
+        total_sum += arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        prefix += arr[i];
+        if (total_sum == 2 * prefix)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main()
 {
-    // create a vector
-    // vector<int> v;
-    // vector<int> v1(5, 2);
+    int n;
+    cout << "Enter the size of array :";
+    cin >> n;
 
-    // cout << "Size of the vector :" << v.size() << endl;
-    // cout << "Capacity of the vector :" << v.capacity() << endl;
-
-    // v.push_back(2);
-    // v.push_back(3);
-    // v.push_back(4);
-    // v1.push_back(7);
-
-    // cout << "Size of the vector :" << v1.size() << endl;
-    // cout << "Capacity of the vector :" << v1.capacity() << endl;
-
-    // Delete the value from the vector
-    //
-
-    // print first and last elements in the vector
-
-    vector<int> arr;
-
-    arr.push_back(34);
-    arr.push_back(65);
-    arr.push_back(312);
-    arr.push_back(44);
-    arr.push_back(64);
-
-    cout << arr[0] << endl;
-    cout << arr.front() << endl;
-    cout << arr[arr.size() - 1] << endl;
-    cout << arr.back() << endl;
-
-    // copy the elements of first vector to another
-
-    // vector<int> a;
-
-    // a = arr;
-    // cout << a.size();
-    // cout << endl;
-
-    // for (auto it = arr.begin(); it != arr.end(); it++)
-    // {
-    //     cout << *it << " ";
-    // }
-    // cout << endl;
-    // for (auto i : arr)
-    //     cout << i << " ";
-
-    // sorting in Vector
-
-    vector<int> ans;
-
-    ans.push_back(10);
-    ans.push_back(78);
-    ans.push_back(40);
-    ans.push_back(3);
-    ans.push_back(13);
-
-    // sort in increasing order
-    sort(ans.begin(), ans.end());
-    for (int i = 0; i < ans.size(); i++)
+    vector<int> v(n);
+    cout << "Enter the element in array :";
+    for (int i = 0; i < n; i++)
     {
-        cout << ans[i] << endl;
-        cout << endl;
+        cin >> v[i];
     }
 
-    // sort in decreasing order
-    cout << "sort in decreasing order" << endl;
-    sort(ans.rbegin(), ans.rend());
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << endl;
-        cout << endl;
-    }
+    cout << divide(v);
 }
