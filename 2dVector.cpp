2982f0a5fd34dcba49f7b0de2deb10436ce9332a
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-11-28 14:36:31 +0530

    wave form

--- 2dVector.cpp
+++ 2dVector.cpp
@@ -4,13 +4,29 @@
 
 using namespace std;
 
+void wave(int arr[][4], int row, int col)
+{
+    for (int j = 0; j < col; j++)
+    {
+        if (j % 2 == 0)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int i = row - 1; i >= 0; i--)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+    }
+}
+
 int main()
 {
-    // create 2d vector
-
-    vector<vector<int>> matrix(3, vector<int>(4, 1));
-
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 4; j++)
-            cout << matrix[i][j] << " ";
+    int arr[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    wave(arr, 3, 4);
 }
